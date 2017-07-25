190fcb321cf843550cbcec3f0c6ccd260b5185af
Zeno Zeng <zenoofzeng@gmail.com>
2017-07-25 17:50:05 +0800

    handler

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # aws-lambda-geoip
+
+AWS Lambda GeoIP Service based on:
+
+- https://github.com/eawsy/aws-lambda-go-shim
+- https://github.com/oschwald/geoip2-golang
+
+## Data
+
+http://dev.maxmind.com/geoip/geoip2/geolite2/
+
+```
+tree data
+data
+└── GeoLite2-City.mmdb
+```
+
+## Build
+
+```bash
+go get -u github.com/jteeuwen/go-bindata/...
+go-bindata -nocompress -o data.go data/
+```
+
+```bash
+docker pull eawsy/aws-lambda-go-shim:latest
+go get -u -d github.com/eawsy/aws-lambda-go-core/...
+make
+```
+
+## Deploy
+
+- Runtime: python2.7
+- Handler: handler.Handle
--- handler.go
+++ handler.go
@@ -1,7 +1,40 @@
 package main
 
-import "github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+import (
+	"encoding/json"
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
+)
 
-func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
-	return "Hello, World!", nil
+func Handle(evt interface{}, ctx *runtime.Context) (interface{}, error) {
+	names := AssetNames()
+	log.Printf("AssetNames: %v", names)
+	data, err := Asset("data/GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := geoip2.FromBytes(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	_, err = json.Marshal(evt)
+	if err != nil {
+		return "", err
+	}
+	ip := net.ParseIP("8.8.8.8")
+	record, err := db.City(ip)
+	if err != nil {
+		return "", err
+	}
+	res := map[string]interface{}{}
+	res[ip.String()] = record
+	return res, err
+}
+
+func main() {
+	res, _ := Handle(nil, nil)
+	log.Printf("Res: %s", res)
 }
